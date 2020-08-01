606b2855c761a6c7f7403e364ce181d054e99004
etkachev <edwardtkachev@gmail.com>
2020-08-01 00:05:00 -0700

    grabbing some projects for testing

--- src/main/kotlin/com/github/etkachev/nxwebstorm/actions/Welcome.kt
+++ src/main/kotlin/com/github/etkachev/nxwebstorm/actions/Welcome.kt
@@ -3,31 +3,26 @@ package com.github.etkachev.nxwebstorm.actions
 import com.intellij.json.JsonLanguage
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.ui.Messages
-import com.intellij.openapi.vfs.VfsUtilCore
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.openapi.vfs.VirtualFileManager
-import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiManager
-import com.intellij.psi.PsiRecursiveElementWalkingVisitor
+import com.google.gson.JsonParser
 
 class Welcome: AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         if (e.project == null) {
             return
         }
-        val file = VirtualFileManager.getInstance().refreshAndFindFileByUrl("nx.json")
+        val proj = e.project!!
+        val file = ProjectRootManager.getInstance(proj).contentRoots[0].findFileByRelativePath("nx.json")
         if (file == null) {
             Messages.showMessageDialog(e.project, "This is no Nx", "Sorry", Messages.getWarningIcon())
             return
         }
         val nxJson = PsiManager.getInstance(e.project!!).findFile(file) ?: return
-        val psi = nxJson.viewProvider.getPsi(JsonLanguage.INSTANCE)
-        psi.accept(object: PsiRecursiveElementWalkingVisitor() {
-            override fun visitElement(element: PsiElement) {
-                super.visitElement(element)
-            }
-        })
-        Messages.showMessageDialog(e.project, "Hello from Nx", "Title Here", Messages.getInformationIcon())
+        val json = JsonParser.parseString(nxJson.text)
+        val projects = json.asJsonObject.getAsJsonObject("projects").keySet()
+        val firstFew = projects.take(5).joinToString(",")
+        Messages.showMessageDialog(e.project, "First projects are $firstFew", "Hello from Nx", Messages.getInformationIcon())
     }
 }

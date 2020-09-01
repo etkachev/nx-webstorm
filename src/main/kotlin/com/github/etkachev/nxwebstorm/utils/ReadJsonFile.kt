f7003d12feca8276444d7989aa6bd896f485f7c5
etkachev <edwardtkachev@gmail.com>
2020-09-01 00:24:08 -0700

    indenting testing

--- src/main/kotlin/com/github/etkachev/nxwebstorm/utils/ReadJsonFile.kt
+++ src/main/kotlin/com/github/etkachev/nxwebstorm/utils/ReadJsonFile.kt
@@ -9,12 +9,12 @@ import com.intellij.openapi.util.ThrowableComputable
 import com.intellij.psi.PsiManager
 
 class ComputeReadJsonFile(private val project: Project, private val filePath: String) :
-        ThrowableComputable<JsonObject, NoSuchElementException> {
+  ThrowableComputable<JsonObject, NoSuchElementException> {
   override fun compute(): JsonObject {
     val file = ProjectRootManager.getInstance(project).contentRoots[0].findFileByRelativePath(filePath)
-            ?: throw NoSuchElementException("Could not find file for $filePath")
+      ?: throw NoSuchElementException("Could not find file for $filePath")
     val fileContents = PsiManager.getInstance(project).findFile(file)
-            ?: throw NoSuchElementException("Could not find file for $filePath")
+      ?: throw NoSuchElementException("Could not find file for $filePath")
     val json = JsonParser.parseString(fileContents.text)
     return json.asJsonObject
   }

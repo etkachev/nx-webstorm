6cea39593ef6d272d9094a2592d00b6c5703459e
etkachev <edwardtkachev@gmail.com>
2020-10-18 20:52:22 -0700

    added nrwl icon to nx tool windwo

--- src/main/kotlin/icons/PluginIcons.java
+++
@@ -1,9 +0,0 @@
-package icons;
-
-import com.intellij.openapi.util.IconLoader;
-
-import javax.swing.*;
-
-public interface PluginIcons {
-    Icon NRWL_ICON = IconLoader.getIcon("/icons/nrwl.svg");
-}
